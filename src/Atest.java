86c7a72593ef850dec5b56dc9bd0337381b24a78
peng <13121125012@163.com>
2021-05-01 18:18:44 +0800

    first commit

--- src/Atest.java
+++ src/Atest.java
@@ -4,5 +4,6 @@ public class Atest {
     public static void main(String[] args)  {
         float f= (float)1e-30;
         System.out.println((double)f);
+        System.out.println(121);
     }
 }
